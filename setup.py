31249e2b1208fe1eb80432eb4f3326e9ee569472
User <nevo.krien@gmail.com>
2023-08-03 15:21:53 +0300

    added setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Your-Project-Name',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'transformers',  # specify the transformers library as a dependency
+    ],
+)
