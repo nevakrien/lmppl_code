d4302b52e86671ced4667fc7723550ff8aa66b17
User <nevo.krien@gmail.com>
2023-08-04 02:18:59 +0300

    ver

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='lmppl_code',
-    version='0.2',
+    version='0.3',
     packages=find_packages(),
     install_requires=[
         'transformers',  # specify the transformers library as a dependency
